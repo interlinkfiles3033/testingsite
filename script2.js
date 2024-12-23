458a32f424d1641d3bb65ddb64ebfcccdd64e3cf
Ethan Huerta <64386903+theinfomaniac@users.noreply.github.com>
2024-12-22 18:42:01 -0600

    mobile sidebar test

--- script2.js
+++ script2.js
@@ -1,40 +1,42 @@
-const openButton = document.getElementById('open-sidebar-button')
-const navbar = document.getElementById('navbar')
+const openButton = document.getElementById('open-sidebar-button');
+const closeButton = document.getElementById('close-sidebar-button');
+const navbar = document.getElementById('navbar');
+const overlay = document.getElementById('overlay');
 
-const media = window.matchMedia("(width < 700px)")
+const media = window.matchMedia("(max-width: 700px)");
 
-media.addEventListener('change', (e) => updateNavbar(e))
+media.addEventListener('change', (e) => updateNavbar(e));
 
-function updateNavbar(e){
-  const isMobile = e.matches
-  console.log(isMobile)
-  if(isMobile){
-    navbar.setAttribute('inert', '')
-  }
-  else{
+function updateNavbar(e) {
+  const isMobile = e.matches;
+  if (isMobile) {
+    navbar.setAttribute('inert', '');
+  } else {
     // desktop device
-    navbar.removeAttribute('inert')
+    navbar.removeAttribute('inert');
   }
 }
 
-function openSidebar(){
-  navbar.classList.add('show')
-  openButton.setAttribute('aria-expanded', 'true')
-  navbar.removeAttribute('inert')
+function openSidebar() {
+  navbar.classList.add('show');
+  openButton.setAttribute('aria-expanded', 'true');
+  navbar.removeAttribute('inert');
+  overlay.style.display = 'block';
 }
 
-function closeSidebar(){
-  navbar.classList.remove('show')
-  openButton.setAttribute('aria-expanded', 'false')
-  navbar.setAttribute('inert', '')
+function closeSidebar() {
+  navbar.classList.remove('show');
+  openButton.setAttribute('aria-expanded', 'false');
+  navbar.setAttribute('inert', '');
+  overlay.style.display = 'none';
 }
 
 // For Bookmark Links
-// const navLinks = document.querySelectorAll('nav a')
-// navLinks.forEach(link => {
-//   link.addEventListener('click', () => {
-//     closeSidebar()
-//   })
-// })
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    closeSidebar();
+  });
+});
 
-updateNavbar(media)
+updateNavbar(media);
