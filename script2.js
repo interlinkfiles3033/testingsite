6deca785489bf269f7872642737448b0a8c073ef
Ethan Huerta <64386903+theinfomaniac@users.noreply.github.com>
2024-12-22 19:18:56 -0600

    revert?

--- script2.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.getElementById('hamburger');
-    const navLinks = document.getElementById('nav-links');
-
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
